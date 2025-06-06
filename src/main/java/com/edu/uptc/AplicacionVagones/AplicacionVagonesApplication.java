106b4c2df78a59795e06f82f73ed8c9772f9b6e1
JuancDuarte <juan.duarte09@uptc.edu.co>
2025-06-05 20:58:55 -0500

    log y cache 05/06/2025

--- src/main/java/com/edu/uptc/AplicacionVagones/AplicacionVagonesApplication.java
+++ src/main/java/com/edu/uptc/AplicacionVagones/AplicacionVagonesApplication.java
@@ -2,8 +2,12 @@ package com.edu.uptc.AplicacionVagones;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
+@EnableCaching
 public class AplicacionVagonesApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/edu/uptc/AplicacionVagones/Controller/VagonetaControl.java
+++ src/main/java/com/edu/uptc/AplicacionVagones/Controller/VagonetaControl.java
@@ -3,11 +3,15 @@ package com.edu.uptc.AplicacionVagones.Controller;
 import java.util.List;
 import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,12 +34,16 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequestMapping("/vagoneta/")
 @CrossOrigin(origins="*")
 public class VagonetaControl {
+private static final Logger logger =LoggerFactory.getLogger(VagonetaControl.class);
 @Autowired
 @Qualifier("vagonService")
 private VagonetaServiceImplementation vsi;
 
 @PutMapping(path = "/añadirMaterial/{id}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Vagoneta> putMaterial(@RequestBody Material material, @PathVariable Long id) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String username = auth.getName();
+        logger.info("El usuario {} accedió al servicio /añadirMaterial", username);
         Vagoneta updatedVagoneta = vsi.addMaterial(id, material);
             if (updatedVagoneta != null) {
                 return ResponseEntity.ok(updatedVagoneta);
@@ -46,6 +54,9 @@ private VagonetaServiceImplementation vsi;
 
 @GetMapping(path = "/Calcularpeso/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<PesoDTO> obtenerCargaVagon(@PathVariable long id) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String username = auth.getName();
+        logger.info("El usuario {} accedió al servicio /Calcular peso", username);
         Vagoneta vagoneta = vsi.getVagonetaById(id);
 
         if (vagoneta != null) {
@@ -67,14 +78,23 @@ private VagonetaServiceImplementation vsi;
     }
 @GetMapping(path="/listarVagonetas",produces= MediaType.APPLICATION_JSON_VALUE)
     public List<Vagoneta> listarVagonetas() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String username = auth.getName();
+        logger.info("El usuario {} accedió al servicio /listar Vagonetas", username);
         return vsi.getListVagon();
     }
 @GetMapping(path="/listarMateriales",produces= MediaType.APPLICATION_JSON_VALUE)
     public List<Material> listarMateriales() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String username = auth.getName();
+        logger.info("El usuario {} accedió al servicio /listar Materiales", username);
         return vsi.getListMaterials();
     }
 @PutMapping(path="/EntradaMina/{id}",  produces = MediaType.APPLICATION_JSON_VALUE)
 public ResponseEntity<Vagoneta> entradaMina( @PathVariable Long id) {
+    Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String username = auth.getName();
+        logger.info("El usuario {} accedió al servicio /Entrada Mina", username);
         Vagoneta updatedVagoneta = vsi.getVagonetaById(id);
             if (updatedVagoneta != null) {
                 vsi.entradaVagon(id, updatedVagoneta.getMaterial());
@@ -85,11 +105,17 @@ public ResponseEntity<Vagoneta> entradaMina( @PathVariable Long id) {
 }
 @PutMapping(path="/salidaMina/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public String salidaVehiculo(@PathVariable Long id) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String username = auth.getName();
+        logger.info("El usuario {} accedió al servicio /salida Mina", username);
         vsi.salidaVagoneta(id);
         return "Vehículo está saliendo. Revisa la consola para el proceso completo.";
 }
 @PostMapping("/vender/{id}")
     public ResponseEntity<String> venderMateriales(@PathVariable Long id) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String username = auth.getName();
+        logger.info("El usuario {} accedió al servicio /vender materiales", username);
         try {
             double total = vsi.venderMateriales(id);
             return ResponseEntity.ok("Materiales vendidos por $" + total);
@@ -99,6 +125,9 @@ public ResponseEntity<Vagoneta> entradaMina( @PathVariable Long id) {
     }
 @GetMapping(path="/buscarVagoneta/{id}",produces= MediaType.APPLICATION_JSON_VALUE)
 public ResponseEntity<Vagoneta> buscarVagoneta(@PathVariable Long id) {
+    Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String username = auth.getName();
+        logger.info("El usuario {} accedió al servicio /buscar vagoneta", username);
     Vagoneta vagoneta = vsi.getVagonetaById(id);
     if (vagoneta != null) {
         return ResponseEntity.ok(vagoneta);
@@ -108,11 +137,17 @@ public ResponseEntity<Vagoneta> buscarVagoneta(@PathVariable Long id) {
 }
 @PostMapping(path="/crearVagoneta", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 public ResponseEntity<Vagoneta> crearVagoneta(@RequestBody Vagoneta vagoneta) {
+    Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String username = auth.getName();
+        logger.info("El usuario {} accedió al servicio /crear vagoneta", username);
     Vagoneta createdVagoneta = vsi.saveVagoneta(vagoneta);
     return ResponseEntity.status(HttpStatus.CREATED).body(createdVagoneta);
 }
 @PostMapping(path="/crearMaterial", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 public ResponseEntity<Material> crearMaterial(@RequestBody Material material) {
+    Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String username = auth.getName();
+        logger.info("El usuario {} accedió al servicio /crear material", username);
     Material createdMaterial = vsi.saveMaterial(material);
     return ResponseEntity.status(HttpStatus.CREATED).body(createdMaterial);
 }
